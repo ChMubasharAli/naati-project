6e774293586a9c247514686045d99cd6f7c37a99
ChMubasharAli <muhammadmubashirali075@gmail.com>
2026-01-15 12:26:40 +0500

    remove server proxy configuration from Vite config

--- vite.config.js
+++ vite.config.js
@@ -5,13 +5,4 @@ import react from "@vitejs/plugin-react";
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react(), tailwindcss()],
-  server: {
-    proxy: {
-      "/api": {
-        target: "https://naati-test.onrender.com",
-        changeOrigin: true,
-        secure: false,
-      },
-    },
-  },
 });
